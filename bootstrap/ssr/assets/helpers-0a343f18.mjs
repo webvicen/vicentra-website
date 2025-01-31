e7915ca73f9cce25ba430253e0dfb76338f7a432
rickydamarsaputra <retrocode.rc@gmail.com>
2025-01-31 09:25:53 +0700

    Menghapus bootstrap/ssr dari Git tracking

--- bootstrap/ssr/assets/helpers-0a343f18.mjs
+++
@@ -1,7 +0,0 @@
-const dateFormatIdn = (tanggal) => {
-  const options = { day: "numeric", month: "long", year: "numeric" };
-  return new Intl.DateTimeFormat("id-ID", options).format(new Date(tanggal));
-};
-export {
-  dateFormatIdn as d
-};
