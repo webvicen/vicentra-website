807dc21b6d2c5a675158d5ff7ec748d24d2344cd
rickydamarsaputra <retrocode.rc@gmail.com>
2025-03-13 09:24:43 +0700

    update: conrs.php

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'build/assets/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://vicentra.co.id', 'https://www.vicentra.co.id'],
 
     'allowed_origins_patterns' => [],
 
