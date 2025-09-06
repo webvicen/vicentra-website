e9ee55e8d1898b1ce1a849db61d02469fc1bc19f
webvicen <web.vicentra@gmail.com>
2025-09-06 10:05:18 +0700

    update import csv

--- app/Imports/RedirectsImport.php
+++ app/Imports/RedirectsImport.php
@@ -10,11 +10,9 @@ class RedirectsImport implements ToModel, WithHeadingRow
 {
     public function model(array $row)
     {
-        Redirect::updateOrCreate(
-            ['old_slug' => $row['old_slug']], // cari berdasarkan old_slug
-            ['new_slug' => $row['new_slug']]  // update atau insert new_slug
+        return Redirect::updateOrCreate(
+            ['old_slug' => $row['old_slug']],
+            ['new_slug' => $row['new_slug']]
         );
-
-        return null; // kita tidak perlu return model baru
     }
 }
